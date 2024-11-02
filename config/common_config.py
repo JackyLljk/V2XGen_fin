4c8be4abe6a1125313259306cb395ef250f2875d
JKey <jacky_ljk@163.com>
2024-11-02 19:50:20 +0800

    update

---
+++ config/common_config.py
@@ -0,0 +1,13 @@
+import os
+occlusion_th = 0.95
+occ_point_max = 20
+
+project_dir = "./"
+
+assets_dir = "{}/_assets".format(project_dir)
+obj_dir_path = "{}/shapenet".format(assets_dir)
+obj_cp_dir = "{}/copy_paste".format(assets_dir)
+
+obj_filename = "models/model_normalized.gltf"
+#
+multi_scale = 5.5
